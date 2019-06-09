8db77b7249aa44611b11f052206a8e0cb4a0ce0c
Tahsin Emirhan Tekoglu <tahsinemirhan58771@gmail.com>
2019-06-09 16:07:03 +0200

    Name changes and some table progress

--- finaltable.py
+++ finaltable.py
@@ -1,45 +1,15 @@
 import csv
-import numpy as np 
+import numpy as np
+import os
 import pandas
+flist =[]
+[flist.append(f.replace('.xml','')) for f in os.listdir('./hmmprofiles') if not f.endswith('.hmm')]
+flist.sort()
 
-with open('test2', 'r') as f:
-	reader = csv.reader(f, delimiter = '\t')
-	with open('ttest.csv', 'w') as n:
-		writer = csv.writer(n, delimiter = ',')
-		writer.writerow(['Cluster'] + ['TA'])
-		for row in reader:
-			writer.writerow([row[1]] + [row[2]])
+ddata = ['Total #seq. elements','Alignment length','# of unknowns','Total hits','Eukaryote hits','Fungi hits','Hits (NACHT/NB-ARC)','1st most abundant pfam ann.','2nd most abundant pfam ann.','3rd most abundant pfam ann.']
+#print flist
+table = pandas.DataFrame(index=flist,columns=ddata)
 
-df = pandas.read_csv('ttest.csv') 
-table = pandas.crosstab(df['Cluster'],df['TA']) 
+print(table)
 
-colmax = table.max(axis=0)				#max of columns (series)
-colmax.name = 'colmax'
-rowmax = table.max(axis=1)				#max of row		(series)
-
-colsum = table.sum(axis=0)				#sum of columns (series)
-colsum.name = 'colsum'										
-rowsum = table.sum(axis=1)				#sum of rows	(series)
-
-rowpercent = (rowmax/rowsum)*100
-colpercent = (colmax/colsum)*100
-colpercent.name = 'colpercent'
-
-temprowwavg = (rowpercent * rowsum).sum() / rowsum.sum()
-tempcolwavg = (colpercent * colsum).sum() / colsum.sum()
-rowwavg = pandas.Series(temprowwavg, name='rowweightedavg', index=[table.index[0]]); print(rowwavg)
-colwavg = pandas.Series(tempcolwavg, name='colweightedavg', index=[table.columns[0]]); print(colwavg)
-
-wrows = table.assign(rowmax = rowmax); #print(rowmax)
-wcols = wrows.append(colmax)
-wrows2 = wcols.assign(rowsum = rowsum)
-wcols2 = wrows2.append(colsum)
-wrows3 = wcols2.assign(rowpercent = rowpercent)
-wcols3 = wrows3.append(colpercent)
-wrows4 = wcols3.assign(rowweightedavg = rowwavg)
-wcols4 = wrows4.append(colwavg)
-
-wcols4.to_csv('newtable.csv', sep='\t')
-
-print(wcols4.index[0])
-print(wcols4.columns[0])
+table.to_csv('./finaltable.csv')
