df9cb50e4015f6d2300897bbb82c7dc9b3dc30f8
Tahsin Emirhan Tekoglu <tahsinemirhan58771@gmail.com>
2019-06-10 14:54:27 +0200

    Formatting some files

--- finaltable.py
+++ finaltable.py
@@ -6,10 +6,36 @@ flist =[]
 [flist.append(f.replace('.xml','')) for f in os.listdir('./hmmprofiles') if not f.endswith('.hmm')]
 flist.sort()
 
-ddata = ['Total #seq. elements','Alignment length','# of unknowns','Total hits','Eukaryote hits','Fungi hits','Hits (NACHT/NB-ARC)','1st most abundant pfam ann.','2nd most abundant pfam ann.','3rd most abundant pfam ann.']
-#print flist
+ddata = ['Total #seq. elements','Alignment length','# of unknowns','1st most abundant ann.','2nd most abundant ann.','3rd   most abundant ann.','Total hits','Eukaryote hits','Fungi hits','Hits (NACHT/NB-ARC)','1st most abundant pfam ann.','2nd most abundant pfam ann.','3rd most abundant pfam ann.']
 table = pandas.DataFrame(index=flist,columns=ddata)
 
-print(table)
+'''
+for allfiles in os.listdir('./clustersfasta/hmmed'):
+    if allfiles.endswith('aligned'): 
+        with open('./clustersfasta/hmmed/'+allfiles,'r') as a:
+            print a.readline()
+'''
+for allfiles in os.listdir('./clustersfasta/hmmed'):
+    if allfiles.endswith('aligned'): 
+        with open('./clustersfasta/hmmed/'+allfiles,'r') as a:
+            stringy = a.read()
+            dashes = stringy.count('-')
+            annotations = stringy.count('>')
+            print dashes
+            print annotations
+            '''
+            for i, line in enumerate(a):
+                if i==0:
+                    print line
+            '''
 
-table.to_csv('./finaltable.csv')
+
+'''
+def add_alignmentlen():
+    for allfiles in os.listdir('./clustersfasta/hmmed'):
+        if f.endswith('aligned'):
+'''
+
+#print(table)
+
+#table.to_csv('./finaltable.csv')
