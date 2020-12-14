e15fc6267da2fea4ca47f70a2e292f8e20d04c64
pauldrinn <tahsinemirhan58771@gmail.com>
2020-12-14 14:36:08 +0300

    Modified the structure

--- cluster_alignment/batch_alignment.sh
+++ cluster_alignment/batch_alignment.sh
@@ -1,8 +1,8 @@
-mkdir -p separated_clusters/aligned_clusters
+mkdir -p aligned_clusters
 
 for FILE in separated_clusters/*.fa; do
 	NAME="${FILE##*/}"
-    OUTPUT="separated_clusters/aligned_clusters/${NAME%%.*}_aligned.fa"
+    OUTPUT="aligned_clusters/${NAME%%.*}_aligned.fa"
 	clustalo -i $FILE -o "$OUTPUT" --auto
     echo "$NAME aligned"
 done
